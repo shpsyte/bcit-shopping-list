e8066384c899e20680d18575eb7f1195c4a22614
jose-luiz <jose.iscosistemas@gmail.com>
2020-05-05 15:56:09 -0700

    Updates

---
+++ precache-manifest.90e1a192f1472a55fee196e336923879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d858938cf90baaea6fdc9a89d789dd9",
+    "url": "/bcit-shopping-list/index.html"
+  },
+  {
+    "revision": "1cf2f91336912acaf46e",
+    "url": "/bcit-shopping-list/static/css/main.e3897297.chunk.css"
+  },
+  {
+    "revision": "a63e94f1118aedc0f656",
+    "url": "/bcit-shopping-list/static/js/2.b5b4886d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bcit-shopping-list/static/js/2.b5b4886d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf2f91336912acaf46e",
+    "url": "/bcit-shopping-list/static/js/main.e2fda5cb.chunk.js"
+  },
+  {
+    "revision": "c77bd5bf94a2d6fad767",
+    "url": "/bcit-shopping-list/static/js/runtime-main.cbb9be50.js"
+  }
+]);
